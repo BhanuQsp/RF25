f91ec37440a010dbbd0a8edbf53ee5ab866a816d
javagalbhanu <javagalbhanu@gmail.com>
2019-02-26 12:21:02 +0530

    Maven Upodates

--- Feb252019/src/qsp/DemoA.java
+++ Feb252019/src/qsp/DemoA.java
@@ -1,5 +1,17 @@
 package qsp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class DemoA {
 
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	@Test
+	public void testA() {
+		WebDriver driver=new ChromeDriver();
+		driver.quit();
+	}
 }
--- Feb252019/src/qsp/Script2.java
+++
@@ -1,8 +0,0 @@
-package qsp;
-
-public class Script2 {
-
-	void testA() {
-		
-	}
-}
